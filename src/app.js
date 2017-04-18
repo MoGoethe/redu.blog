7384fec8d3b39d5697798faebd5ddd8c6234697f
mogoethe <1095364870@qq.com>
2017-04-18 14:30:46 +0800

    start

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { render } from "react-dom"
+import Redux,{ createStore ,applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+import createLogger from "redux-logger"
+import { Provider } from "react-redux"
+import Reducer from "./reducers/index.jsx"
+import App from "./containers/index.jsx"
+
+const middleware = [ thunk ]
+process.env.NODE_ENV !== 'production' ? middleware.push(createLogger()) : ''
+
+console.log(middleware);
+
+const store = createStore(Reducer,applyMiddleware(...middleware))
+
+render(
+	<Provider store={ store }>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+)
+
